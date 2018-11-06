aed34f55f876f9f776f403a78a7481cd111a39b3
Avinash.Komaturi <aviavinash0464@gmail.com>
2018-11-06 20:25:21 +0530

    add db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose =require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://<avinash>:<abcd1234>@ds253203.mlab.com:53203/agile-beyond");
+mongoose.connect("mongodb://<avinash>:<abcd1234>@ds253203.mlab.com:53203/agile-beyond"||"mongodb://localhost:27017");
 
 module.exports = {mongoose};
